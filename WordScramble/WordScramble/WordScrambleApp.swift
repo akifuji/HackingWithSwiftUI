bbcff2a85ff68e48139e617ae86b1ce8377aeded
Akifumi Fujita <akifuji@protonmail.com>
2021-05-18 21:36:14 +0900

    DAY 30

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Akifumi Fujita on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
