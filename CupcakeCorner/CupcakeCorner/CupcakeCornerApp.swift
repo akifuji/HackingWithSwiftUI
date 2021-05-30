127a1b2a19acf915de3d5d379b19db1bfcabff49
Akifumi Fujita <akifuji@protonmail.com>
2021-05-30 21:33:12 +0900

    DAY 50

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Akifumi Fujita on 2021/05/30.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
