50ee85b6a96e23b2d8085d960e58554851a42b99
Akifumi Fujita <akifuji@protonmail.com>
2021-06-13 11:55:03 +0900

    DAY 73

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -11,40 +11,30 @@ import MapKit
 
 struct ContentView: View {
     @State private var centerCoordinate = CLLocationCoordinate2D()
-    @State private var isUnlocked = false
-    @State private var locations = [MKPointAnnotation]()
+    @State private var locations = [CodableMKPointAnnotation]()
     @State private var selectedPlace: MKPointAnnotation?
     @State private var showingPlaceDetails = false
     @State private var showingEditScreen = false
+    @State private var isUnlocked = false
+    //@State private var isUnlocked = true
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
     
     var body: some View {
         ZStack {
-            MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
-                .edgesIgnoringSafeArea(.all)
-            Circle()
-                .fill(Color.blue)
-                .opacity(3)
-                .frame(width: 32, height: 32)
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        let newLocation = MKPointAnnotation()
-                        newLocation.coordinate = centerCoordinate
-                        newLocation.title = "Example location"
-                        locations.append(newLocation)
-                        selectedPlace = newLocation
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                    .padding()
-                    .background(Color.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
+            if isUnlocked {
+                MapView(centerCoordinate: $centerCoordinate, selectedPlace: $selectedPlace, showingPlaceDetails: $showingPlaceDetails, annotations: locations)
+                    .edgesIgnoringSafeArea(.all)
+                BlueCircle()
+                FloatingPlusButton(locations: $locations, selectedPlace: $selectedPlace, centerCoordinate: $centerCoordinate)
+            } else {
+                Button("Unlock places") {
+                    authenticate()
                 }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
             }
         }
         .alert(isPresented: $showingPlaceDetails) {
@@ -52,16 +42,40 @@ struct ContentView: View {
                 showingEditScreen = true
             })
         }
-        .sheet(isPresented: $showingEditScreen) {
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Authemtication error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: saveData) {
             if let selectedPlace = selectedPlace {
                 EditView(placemark: selectedPlace)
             }
         }
+        .onAppear(perform: loadData)
     }
     
     func getDocumentsDirectory() -> URL {
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return paths[0]
+    }
+    
+    func loadData() {
+        let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+        do {
+            let data = try Data(contentsOf: filename)
+            locations = try JSONDecoder().decode([CodableMKPointAnnotation].self, from: data)
+        } catch {
+            print("Unable to load saved data")
+        }
+    }
+    
+    func saveData () {
+        do {
+            let filename = getDocumentsDirectory().appendingPathComponent("SavedPlaces")
+            let data = try JSONEncoder().encode(locations)
+            try data.write(to: filename, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data")
+        }
     }
     
     func authenticate() {
@@ -76,31 +90,59 @@ struct ContentView: View {
                     if success {
                         isUnlocked = true
                     } else {
-                        
+                        alertMessage = "Unable to authenticate"
+                        showingAlert = true
                     }
                 }
             }
         } else {
-            
+            alertMessage = "Unable to detect biometrics authentication"
+            showingAlert = true
+        }
+    }
+    
+    struct BlueCircle: View {
+        var body: some View {
+            Circle()
+                .fill(Color.blue)
+                .opacity(3)
+                .frame(width: 32, height: 32)
+        }
+    }
+    
+    struct FloatingPlusButton: View {
+        @Binding var locations: [CodableMKPointAnnotation]
+        @Binding var selectedPlace: MKPointAnnotation?
+        @Binding var centerCoordinate: CLLocationCoordinate2D
+        
+        var body: some View {
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = CodableMKPointAnnotation()
+                        newLocation.coordinate = centerCoordinate
+                        newLocation.title = "Example location"
+                        locations.append(newLocation)
+                        selectedPlace = newLocation
+                    }) {
+                        Image(systemName: "plus")
+                            .padding()
+                            .background(Color.black.opacity(0.75))
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .clipShape(Circle())
+                            .padding(.trailing)
+                    }
+                }
+            }
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
-extension FileManager {
-    static func save(_ file: String) {
-        let url = self.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        do {
-            try file.write(to: url, atomically: true, encoding: .utf8)
-            let input = try String(contentsOf: url)
-            print(input)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
